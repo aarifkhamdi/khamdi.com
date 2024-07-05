65ed9c3955bffedc906b9ff02253f9db2567ff60
aarifkhamdi <aarifkhamdi@gmail.com>
2024-07-05 10:57:25 +0500

    add analytics

--- apps/ch1/src/app/layout.tsx
+++ apps/ch1/src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import './global.css';
 
 export const metadata = {
@@ -11,7 +13,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
